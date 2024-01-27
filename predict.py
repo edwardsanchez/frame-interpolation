5b79cbc3e883c28d9ebbbc7d00cddd9a430d31fe
Edward Sanchez <edwardsanchez@mac.com>
2024-01-26 18:35:05 -0800

    New predict.py file

--- predict.py
+++ predict.py
@@ -11,7 +11,6 @@ from eval import interpolator, util
 
 _UINT8_MAX_F = float(np.iinfo(np.uint8).max)
 
-
 class Predictor(cog.Predictor):
     def setup(self):
         import tensorflow as tf
@@ -20,6 +19,9 @@ class Predictor(cog.Predictor):
 
         # Batched time.
         self.batch_dt = np.full(shape=(1,), fill_value=0.5, dtype=np.float32)
+
+        # Add the _OUTPUT_VIDEO flag
+        self.output_video = False
 
     @cog.input(
         "frame1",
@@ -37,52 +39,52 @@ class Predictor(cog.Predictor):
         default=1,
         min=1,
         max=8,
-        help="Controls the number of times the frame interpolator is invoked If set to 1, the output will be the "
+        help="Controls the number of times the frame interpolator is invoked. If set to 1, the output will be the "
              "sub-frame at t=0.5; when set to > 1, the output will be the interpolation video with "
              "(2^times_to_interpolate + 1) frames, fps of 30.",
     )
-    def predict(self, frame1, frame2, times_to_interpolate):
+    @cog.input(
+        "output_video",
+        type=bool,
+        default=False,
+        help="Set to false to get an array of image paths instead of a video."
+    )
+    def predict(self, frame1, frame2, times_to_interpolate, output_video):
         INPUT_EXT = ['.png', '.jpg', '.jpeg']
         assert os.path.splitext(str(frame1))[-1] in INPUT_EXT and os.path.splitext(str(frame2))[-1] in INPUT_EXT, \
-            "Please provide png, jpg or jpeg images."
+            "Please provide png, jpg, or jpeg images."
 
-        # make sure 2 images are the same size
+        # Ensure the two images are the same size
         img1 = Image.open(str(frame1))
         img2 = Image.open(str(frame2))
-        if not img1.size == img2.size:
-            img1 = img1.crop((0, 0, min(img1.size[0], img2.size[0]), min(img1.size[1], img2.size[1])))
-            img2 = img2.crop((0, 0, min(img1.size[0], img2.size[0]), min(img1.size[1], img2.size[1])))
-            frame1 = 'new_frame1.png'
-            frame2 = 'new_frame2.png'
+        if img1.size != img2.size:
+            min_width, min_height = min(img1.size[0], img2.size[0]), min(img1.size[1], img2.size[1])
+            img1 = img1.crop((0, 0, min_width, min_height))
+            img2 = img2.crop((0, 0, min_width, min_height))
+            frame1, frame2 = 'new_frame1.png', 'new_frame2.png'
             img1.save(frame1)
             img2.save(frame2)
-
-        if times_to_interpolate == 1:
-            # First batched image.
-            image_1 = util.read_image(str(frame1))
-            image_batch_1 = np.expand_dims(image_1, axis=0)
-    
-            # Second batched image.
-            image_2 = util.read_image(str(frame2))
-            image_batch_2 = np.expand_dims(image_2, axis=0)
-    
-            # Invoke the model once.
-            
-            mid_frame = self.interpolator.interpolate(image_batch_1, image_batch_2, self.batch_dt)[0]
-            out_path = Path(tempfile.mkdtemp()) / "out.png"
-            util.write_image(str(out_path), mid_frame)
-            return out_path
-
 
         input_frames = [str(frame1), str(frame2)]
 
         frames = list(
             util.interpolate_recursively_from_files(
                 input_frames, times_to_interpolate, self.interpolator))
-        print('Interpolated frames generated, saving now as output video.')
 
-        ffmpeg_path = util.get_ffmpeg_path()
-        mediapy.set_ffmpeg(ffmpeg_path)
-        out_path = Path(tempfile.mkdtemp()) / "out.mp4"
-        mediapy.write_video(str(out_path), frames, fps=30)
-        return out_path
+        if not output_video:
+            # Code to handle saving frames as images and returning their paths
+            image_paths = []
+            temp_dir = tempfile.mkdtemp()
+            for idx, frame in enumerate(frames):
+                image_path = Path(temp_dir) / f'frame_{idx:03d}.png'
+                util.write_image(str(image_path), frame)
+                image_paths.append(str(image_path))
+            return image_paths
+        else:
+            # Existing code to handle video generation
+            print('Interpolated frames generated, saving now as output video.')
+            ffmpeg_path = util.get_ffmpeg_path()
+            mediapy.set_ffmpeg(ffmpeg_path)
+            out_path = Path(tempfile.mkdtemp()) / "out.mp4"
+            mediapy.write_video(str(out_path), frames, fps=30)
+            return out_path
