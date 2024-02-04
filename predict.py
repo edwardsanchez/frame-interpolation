24480930c8893ce49a073be3b8519cc75e3e2592
Fernando Torcelly <fernando@torcelly.com>
2024-02-04 18:46:39 +0000

    Returns replicate urls of the interpolated frames

--- predict.py
+++ predict.py
@@ -1,19 +1,19 @@
 import os
+import io
 from pathlib import Path
 import numpy as np
 import tempfile
 import tensorflow as tf
-import mediapy
-import base64
-from io import BytesIO
 from PIL import Image
 from typing import Any
+from typing import List
 from cog import BasePredictor, Input, Path
 import cog
 
 from eval import interpolator, util
 
 _UINT8_MAX_F = float(np.iinfo(np.uint8).max)
+
 
 class Predictor(BasePredictor):
     def setup(self):
@@ -24,65 +24,52 @@ class Predictor(BasePredictor):
         # Batched time.
         self.batch_dt = np.full(shape=(1,), fill_value=0.5, dtype=np.float32)
 
-    def predict(self, frame1: str = Input(description="The first input frame in base64"), frame2: str = Input(description="The second input frame in base64"), times_to_interpolate: int = Input(description="times_to_interpolate", default=1)) -> Any:
-        INPUT_EXT = ['.png', '.jpg', '.jpeg']
-        assert os.path.splitext(str(frame1))[-1] in INPUT_EXT and os.path.splitext(str(frame2))[-1] in INPUT_EXT, \
-            "Please provide png, jpg or jpeg images."
+    def predict(self, frame1: cog.File = Input(description="The first input frame in a file"), frame2: cog.File = Input(description="The second input frame in a file"), times_to_interpolate: int = Input(description="The second input frame", default=1)) -> Any:
 
-        #Get face image
-        try:
-            img1_decoded = base64.b64decode(frame1)
-            img1_bytesio = BytesIO(img1_decoded)
-            img1 = Image.open(img1_bytesio)
+        # Open files
+        image_1 = Image.open(frame1)
+        image_2 = Image.open(frame2)
 
-            img2_decoded = base64.b64decode(frame2)
-            img2_bytesio = BytesIO(img2_decoded)
-            img2 = Image.open(img2_bytesio)
-        except Exception as e:
-            print("Error reading the image from the body:", str(e))
-            body = {
-                "message": "Error reading the image from the body",
-                "error": str(e)
-            }
-            return {
-                'headers': { "Content-Type": "application/json" },
-                'statusCode': 500,
-                'body': json.dumps(body)
-            }
-
-        # make sure 2 images are the same size
-        if not img1.size == img2.size:
-            img1 = img1.crop((0, 0, min(img1.size[0], img2.size[0]), min(img1.size[1], img2.size[1])))
-            img2 = img2.crop((0, 0, min(img1.size[0], img2.size[0]), min(img1.size[1], img2.size[1])))
+        if not image_1.size == image_2.size:
+            img1 = image_1.crop((0, 0, min(image_1.size[0], image_2.size[0]), min(image_1.size[1], image_2.size[1])))
+            img2 = image_2.crop((0, 0, min(image_1.size[0], image_2.size[0]), min(image_1.size[1], image_2.size[1])))
             frame1 = 'new_frame1.jpg'
             frame2 = 'new_frame2.jpg'
             img1.save(frame1)
             img2.save(frame2)
 
-        if times_to_interpolate == 1:
-            # First batched image.
-            image_1 = util.read_image(str(frame1))
-            image_batch_1 = np.expand_dims(image_1, axis=0)
-    
-            # Second batched image.
-            image_2 = util.read_image(str(frame2))
-            image_batch_2 = np.expand_dims(image_2, axis=0)
-    
-            # Invoke the model once.
-            mid_frame = self.interpolator.interpolate(image_batch_1, image_batch_2, self.batch_dt)[0]
-            out_path = Path(tempfile.mkdtemp()) / "out.jpg"
-            util.write_image(str(out_path), mid_frame)
-            return out_path
+        input_frames = [self.image_to_file_path(image_1), self.image_to_file_path(image_2)]
 
-        input_frames = [str(frame1), str(frame2)]
-
-        frames = list(
+        # Save interpolated frames to disk
+        interpolated_frames = list(
             util.interpolate_recursively_from_files(
                 input_frames, times_to_interpolate, self.interpolator))
-        print('Interpolated frames generated, saving now as output video.')
 
-        ffmpeg_path = util.get_ffmpeg_path()
-        mediapy.set_ffmpeg(ffmpeg_path)
-        out_path = Path(tempfile.mkdtemp()) / "out.mp4"
-        mediapy.write_video(str(out_path), frames, fps=30)
-        return out_path
+        urls = []
+        output_dir = Path(tempfile.mkdtemp())
+        os.makedirs(output_dir, exist_ok=True)
+
+        for i, img in enumerate(interpolated_frames):
+            out_path = os.path.join(output_dir, f"image_{i}.jpg")
+            util.write_image(str(out_path), img)
+            urls.append(Path(out_path))
+
+        # Return the URLs of interpolated frames
+        return urls
+
+    def image_to_file_path(self, image):
+        # Read data from the io.BytesIO object
+        bytes_io = io.BytesIO()
+        image.save(bytes_io, format='PNG')
+        #bytes_io.seek(0)
+        bytes_data = bytes_io.getvalue()
+
+        # Create a temporary file and write the data
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(bytes_data)
+            temp_file_path = temp_file.name
+    
+        # Convert the path of the temporary file to a string
+        file_path_string = str(temp_file_path)
+    
+        return file_path_string
